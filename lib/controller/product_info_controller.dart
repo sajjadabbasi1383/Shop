871ed7d17c69e25f37dabad33179a559ae6a1171
sajjadabbasi1383 <sajjadabbas0083@gmail.com>
2024-01-02 15:37:21 +0330

    Add & Remove function for BasketList

--- lib/controller/product_info_controller.dart
+++ lib/controller/product_info_controller.dart
@@ -1,13 +1,24 @@
 import 'package:get/get.dart';
+import 'package:shop/controller/home_controller.dart';
 import 'package:shop/models/product_model.dart';
 
 import '../services/dio_service.dart';
 
+HomeController homeController=Get.put(HomeController());
 
 class ProductInfoController extends GetxController{
   RxBool loading=false.obs;
   RxInt id=RxInt(0);
   Rx<ProductModel> productInfoModel=ProductModel().obs;
+  RxList<ProductModel> basketList=RxList();
+
+  addProductToBasket(ProductModel productModel){
+    basketList.add(productModel);
+  }
+
+  removeProductFromBasket(index){
+    basketList.removeAt(index);
+  }
 
   getProductInfo() async {
     productInfoModel=ProductModel().obs;
